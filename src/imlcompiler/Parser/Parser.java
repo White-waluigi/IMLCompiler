15facded3ee4a0f8e46fa09f8568f2230a89c311
Andreas-Keller <andreaskeller2010@gmail.com>
2018-10-30 18:40:36 +0100

    expr and cmd

--- src/imlcompiler/Parser/Parser.java
+++ src/imlcompiler/Parser/Parser.java
@@ -40,7 +40,7 @@ public class Parser {
         else
         {
             throw new ParserErrorException("terminal expected: " + terminal +
-                    ", terminal found: " + this.tokenTerminal);
+                    ", terminal found: " + this.tokenTerminal + "  " + this.token.toString());
         }
     }
 
@@ -63,9 +63,9 @@ public class Parser {
     }
 
     /*
-    cpsCmd ::= cmd {SEMICOLON cmd} [[DONE]]
+    cpsCmd ::= cmd {SEMICOLON cmd}
      */
-    private ImlComponent cpsCmd(){
+    private ImlComponent cpsCmd(){ // [[DONE]]
         ImlComponent cpsCmd = new ImlComposite("cpsCmd");
         cpsCmd.add(cmd());
         if (this.tokenTerminal == Terminal.SEMICOLON) {
@@ -110,11 +110,55 @@ public class Parser {
             cmd.add(new ImlItem(consume(Terminal.ENDWHILE)));
             return cmd;
         }
-        //todo other grammar
+        else if (this.tokenTerminal == Terminal.CALL) {
+            cmd.add(new ImlItem(consume(Terminal.CALL)));
+            cmd.add(new ImlItem(consume(Terminal.IDENT)));
+            cmd.add(exprList());
+            //todo [globInits]
+            return cmd;
+        }
+        else if (this.tokenTerminal == Terminal.DEBUGIN){
+            cmd.add(new ImlItem(consume(Terminal.DEBUGIN)));
+            cmd.add(expr());
+            return cmd;
+        }
+        else if (this.tokenTerminal == Terminal.DEBUGOUT){
+            cmd.add(new ImlItem(consume(Terminal.DEBUGOUT)));
+            cmd.add(expr());
+            return cmd;
+        }
         else {
-            throw new ParserErrorException("Error in cmd()");
+            cmd.add(expr());
+            cmd.add(new ImlItem(consume(Terminal.BECOMES)));
+            cmd.add(expr());
+            return cmd;
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /*
         expr    ::= term1 {BOOLOPR term1}
@@ -122,15 +166,14 @@ public class Parser {
         term2  ::= term3 {ADDOPR term3}
         term3  ::= factor {MULTOPR factor}
         factor  ::= LITERAL
-                    | IDENT [INIT
-                    | exprList]
+                    | IDENT [INIT | exprList]
                     | monadicOpr factor
                     | LPAREN expr RPAREN
 
         exprList ::= LPAREN [expr {COMMA expr}] RPAREN
         monadicOpr ::= NOT | ADDOPR
         */
-    private ImlComponent expr(){
+    private ImlComponent expr(){   // [[DONE]]
         ImlComponent expr = new ImlComposite("expr");
         expr.add(term1());
         if (this.tokenTerminal == Terminal.BOOLOPR){
@@ -142,11 +185,101 @@ public class Parser {
         return expr;
     }
 
-    private ImlComponent term1(){
+    private ImlComponent term1(){  // [[DONE]]
         ImlComponent term1 = new ImlComposite("term1");
-        //todo finish term1
+        term1.add(term2());
+        if (this.tokenTerminal == Terminal.RELOPR){
+            term1.add(new ImlItem(consume(Terminal.RELOPR)));
+            term1.add(term2());
+        }
         return term1;
     }
+
+    private ImlComponent term2(){ //[[DONE]]
+        ImlComponent term2 = new ImlComposite("term2");
+        term2.add(term3());
+        if (this.tokenTerminal == Terminal.ADDOPR){
+            while(this.tokenTerminal == Terminal.ADDOPR){
+                term2.add(new ImlItem(consume(Terminal.ADDOPR)));
+                term2.add(term3());
+            }
+        }
+        return term2;
+    }
+
+    private ImlComponent term3(){ //[[DONE]]
+        ImlComponent term3 = new ImlComposite("term3");
+        term3.add(factor());
+        if (this.tokenTerminal == Terminal.MULTOPR){
+            while(this.tokenTerminal == Terminal.MULTOPR){
+                term3.add(new ImlItem(consume(Terminal.MULTOPR)));
+                term3.add(factor());
+            }
+        }
+        return factor();
+    }
+
+    private ImlComponent factor(){ //[[DONE]]
+        ImlComponent factor = new ImlComposite("factor");
+        if (this.tokenTerminal == Terminal.LITERAL){
+            factor.add(new ImlItem(consume(Terminal.LITERAL)));
+        }
+        else if(this.tokenTerminal == Terminal.IDENT){
+            factor.add(new ImlItem(consume(Terminal.IDENT)));
+            if (this.tokenTerminal == Terminal.INIT){
+                factor.add(new ImlItem(consume(Terminal.INIT)));
+            }
+            else if (this.tokenTerminal == Terminal.LPAREN){
+                factor.add(exprList());
+            }
+            else {}
+        }
+        else if (this.tokenTerminal == Terminal.LPAREN){
+            factor.add(new ImlItem(consume(Terminal.LPAREN)));
+            factor.add(expr());
+            factor.add(new ImlItem(consume(Terminal.RPAREN)));
+        }
+        else if (this.tokenTerminal == Terminal.NOTOPR || this.tokenTerminal == Terminal.ADDOPR){
+            factor.add(monadicOpr());
+            factor.add(factor());
+        }
+        else{}
+        return factor;
+    }
+    //exprList ::= LPAREN [expr {COMMA expr}] RPAREN
+    private ImlComponent exprList(){ //[[DONE]]
+        ImlComponent exprList = new ImlComposite("exprList");
+        exprList.add(new ImlItem(consume(Terminal.LPAREN)));
+
+        if (this.tokenTerminal != Terminal.RPAREN){
+            exprList.add(expr());
+            if(this.tokenTerminal == Terminal.COMMA) {
+                while (this.tokenTerminal == Terminal.COMMA) {
+                    exprList.add(new ImlItem(consume(Terminal.COMMA)));
+                    exprList.add(expr());
+                }
+            }
+            //else throw new ParserErrorException("error in exprList");
+        }
+        exprList.add(new ImlItem(consume(Terminal.RPAREN)));
+        return exprList;
+    }
+
+    private ImlComponent monadicOpr(){ //[[DONE]]
+        ImlComponent monadicOpr = new ImlComposite("monadicOpr");
+        if (this.tokenTerminal == Terminal.NOTOPR) {   //should be Terminal.NOT
+            monadicOpr.add(new ImlItem(consume(Terminal.NOTOPR)));
+        }
+        else if (this.tokenTerminal == Terminal.ADDOPR){
+            monadicOpr.add(new ImlItem(consume(Terminal.ADDOPR)));
+
+        }
+        else {
+            throw new ParserErrorException("Error at monadicOpr");
+        }
+        return monadicOpr;
+    }
+
 
 
 }
