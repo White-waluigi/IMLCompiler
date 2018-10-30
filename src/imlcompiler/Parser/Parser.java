7a0bc38648cda1bc2e19ee37105f0914dc18f87f
Andreas-Keller <andreaskeller2010@gmail.com>
2018-10-30 15:04:07 +0100

    continuing on Parser

--- src/imlcompiler/Parser/Parser.java
+++ src/imlcompiler/Parser/Parser.java
@@ -44,9 +44,8 @@ public class Parser {
         }
     }
 
-
     /*
-    *   program ::= PROGRAM IDENT progParamList [GLOBAL cpsDecl] DO cpsCmd ENDPROGRAM
+    program ::= PROGRAM IDENT progParamList [GLOBAL cpsDecl] DO cpsCmd ENDPROGRAM
     */
     private ImlComponent program(){
 
@@ -54,8 +53,8 @@ public class Parser {
 
         program.add(new ImlItem(consume(Terminal.PROGRAM)));
         program.add(new ImlItem(consume(Terminal.IDENT)));
-        //progParamList
-        //[GLOBAL cpsDecl]
+        //todo progParamList
+        //todo [GLOBAL cpsDecl]
         program.add(new ImlItem(consume(Terminal.DO)));
         program.add(cpsCmd());
         program.add(new ImlItem(consume(Terminal.ENDPROGRAM)));
@@ -63,27 +62,91 @@ public class Parser {
         return program;
     }
 
-    //cpsCmd ::= cmd {SEMICOLON cmd}
+    /*
+    cpsCmd ::= cmd {SEMICOLON cmd} [[DONE]]
+     */
     private ImlComponent cpsCmd(){
         ImlComponent cpsCmd = new ImlComposite("cpsCmd");
         cpsCmd.add(cmd());
-        cpsCmd.add(new ImlItem(consume(Terminal.SEMICOLON)));
-        cpsCmd.add(cmd());
+        if (this.tokenTerminal == Terminal.SEMICOLON) {
+            while (this.tokenTerminal == Terminal.SEMICOLON) {
+                cpsCmd.add(new ImlItem(consume(Terminal.SEMICOLON)));
+                cpsCmd.add(cmd());
+            }
+        }
         return cpsCmd;
     }
 
     /*
         cmd ::= SKIP
             | expr BECOMES expr
-            | IF expr THEN cpsCmd ELSE cpsCmd ENDIF | WHILE expr DO cpsCmd ENDWHILE
+            | IF expr THEN cpsCmd ELSE cpsCmd ENDIF
+            | WHILE expr DO cpsCmd ENDWHILE
             | CALL IDENT exprList [globInits]
             | DEBUGIN expr
             | DEBUGOUT expr
     */
     private ImlComponent cmd(){
         ImlComponent cmd = new ImlComposite("cmd");
-        cmd.add(new ImlItem(consume(Terminal.SKIP)));
-
-        return cmd;
+        if(this.tokenTerminal == Terminal.SKIP) {
+            cmd.add(new ImlItem(consume(Terminal.SKIP)));
+            return cmd;
+        }
+        else if(this.tokenTerminal == Terminal.IF){
+            cmd.add(new ImlItem(consume(Terminal.IF)));
+            cmd.add(expr());
+            cmd.add(new ImlItem(consume(Terminal.THEN)));
+            cmd.add(cpsCmd());
+            cmd.add(new ImlItem(consume(Terminal.ELSE)));
+            cmd.add(cpsCmd());
+            cmd.add(new ImlItem(consume(Terminal.ENDIF)));
+            return cmd;
+        }
+        else if (this.tokenTerminal == Terminal.WHILE){
+            cmd.add(new ImlItem(consume(Terminal.WHILE)));
+            cmd.add(expr());
+            cmd.add(new ImlItem(consume(Terminal.DO)));
+            cmd.add(cpsCmd());
+            cmd.add(new ImlItem(consume(Terminal.ENDWHILE)));
+            return cmd;
+        }
+        //todo other grammar
+        else {
+            throw new ParserErrorException("Error in cmd()");
+        }
     }
+
+    /*
+        expr    ::= term1 {BOOLOPR term1}
+        term1  ::= term2 [RELOPR term2]
+        term2  ::= term3 {ADDOPR term3}
+        term3  ::= factor {MULTOPR factor}
+        factor  ::= LITERAL
+                    | IDENT [INIT
+                    | exprList]
+                    | monadicOpr factor
+                    | LPAREN expr RPAREN
+
+        exprList ::= LPAREN [expr {COMMA expr}] RPAREN
+        monadicOpr ::= NOT | ADDOPR
+        */
+    private ImlComponent expr(){
+        ImlComponent expr = new ImlComposite("expr");
+        expr.add(term1());
+        if (this.tokenTerminal == Terminal.BOOLOPR){
+            while (this.tokenTerminal == Terminal.BOOLOPR){
+                expr.add(new ImlItem(consume(Terminal.BOOLOPR)));
+                expr.add(term1());
+            }
+        }
+        return expr;
+    }
+
+    private ImlComponent term1(){
+        ImlComponent term1 = new ImlComposite("term1");
+        //todo finish term1
+        return term1;
+    }
+
+
 }
